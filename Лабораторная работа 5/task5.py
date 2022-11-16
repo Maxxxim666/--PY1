886cab1f12e44295bce797d65d76f063461cac6c
Maxxxim666 <115407408+Maxxxim666@users.noreply.github.com>
2022-11-16 20:55:25 +0300

    Update task5.py

--- Лабораторная работа 5/task5.py
+++ Лабораторная работа 5/task5.py
@@ -2,7 +2,7 @@ from random import sample
 
 
 def get_random_password(n=8) -> str:  # TODO написать функцию генерации случайных паролей
-    all_symbols = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ') + list('abcdefghijklmnopqrstuvwxyz') + list('0123456789')
+    all_symbols = list(string.ascii_uppercase) + list(string.ascii_lowercase) + list(string.digits)
     list_symbol = sample(all_symbols, n)
     password = ''.join(list_symbol)
     return password
